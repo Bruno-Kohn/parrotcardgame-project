f3120e0998a5dc771bb81e1810f1aaa99f8b1075
Bruno Kohn <brunokohn1993@gmail.com>
2021-07-27 15:33:47 -0300

    prompt em while implementado

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
+let numberOfCards = null;
 
+cardsDefinition();
+
+function cardsDefinition() {
+  while (numberOfCards % 2 !== 0 || numberOfCards >= 15 || numberOfCards <= 3) {
+    numberOfCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
+  }
+}
