dd9d89d415eab97bcf05ada657dc65c0b5b7688a
Bruno Kohn <brunokohn1993@gmail.com>
2021-07-27 21:55:38 -0300

    renderização das cartas randomizadas

--- scripts.js
+++ scripts.js
@@ -8,14 +8,36 @@ function cardsDefinition() {
   }
 }
 
-alert(numberOfCards);
+const gifs = [
+  "bobrossparrot.gif",
+  "explodyparrot.gif",
+  "fiestaparrot.gif",
+  "metalparrot.gif",
+  "revertitparrot.gif",
+  "tripletsparrot.gif",
+  "unicornparrot.gif",
+];
+
+const newGifs = [];
+for (let i = 0; i < numberOfCards / 2; i++) {
+  newGifs.push(gifs[i]);
+  newGifs.push(gifs[i]);
+}
+
+newGifs.sort(comparador);
+
+function comparador() {
+  return Math.random() - 0.5;
+}
+
+console.log(newGifs);
 
 const cardsDistribution = document.querySelector(".container");
-console.log(cardsDistribution);
-for (let i = 1; i <= numberOfCards; i++) {
+//console.log(cardsDistribution);
+for (let i = 0; i < numberOfCards; i++) {
   cardsDistribution.innerHTML += `
     <div class="card">
-        <img src="images/front.png" alt="card" />
+        <img src="images/${newGifs[i]}" alt="card" />
     </div>
   `;
 }
