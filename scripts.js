6302f9b404616b2fb56562984796c58b9b6ce4e0
Bruno Kohn <brunokohn1993@gmail.com>
2021-07-30 14:12:22 -0300

    contagem de jogadas implementada

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
 let numberOfCards = null;
-
-cardsDefinition();
-
-function cardsDefinition() {
-  while (numberOfCards % 2 !== 0 || numberOfCards >= 15 || numberOfCards <= 3) {
-    numberOfCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
-  }
-}
-
+let deck = [];
+let firstCard = null;
+let clicks = 0;
+let pairs = 0;
 const gifs = [
   "bobrossparrot.gif",
   "explodyparrot.gif",
@@ -18,26 +13,83 @@ const gifs = [
   "unicornparrot.gif",
 ];
 
-const newGifs = [];
-for (let i = 0; i < numberOfCards / 2; i++) {
-  newGifs.push(gifs[i]);
-  newGifs.push(gifs[i]);
+memoryGame();
+
+function memoryGame() {
+  while (numberOfCards % 2 !== 0 || numberOfCards >= 15 || numberOfCards <= 3) {
+    numberOfCards = parseInt(prompt("Com quantas cartas deseja jogar?"));
+  }
+  console.log(numberOfCards);
+  for (let i = 0; i < numberOfCards / 2; i++) {
+    const card = `<div class="cards card1" id="checkcard${i}" onclick="toSelectCard(this)">
+     <img class="front" src="Images/front.png">
+     <img class="back hide" src="Images/${gifs[i]}">
+     </div>`;
+    deck.push(card);
+    deck.push(card);
+  }
 }
 
-newGifs.sort(comparador);
+deck.sort(comparador);
 
 function comparador() {
   return Math.random() - 0.5;
 }
 
-console.log(newGifs);
-
 const cardsDistribution = document.querySelector(".container");
-//console.log(cardsDistribution);
+cardsDistribution.innerHTML = "";
+
 for (let i = 0; i < numberOfCards; i++) {
-  cardsDistribution.innerHTML += `
-    <div class="card">
-        <img src="images/${newGifs[i]}" alt="card" />
-    </div>
-  `;
+  cardsDistribution.innerHTML += deck[i];
+}
+
+function toSelectCard(card) {
+  const front = card.querySelector(".front");
+  const back = card.querySelector(".back");
+  front.classList.add("hide");
+  back.classList.remove("hide");
+  toCompare(card);
+}
+
+function toCompare(card) {
+  if (firstCard === null) {
+    console.log("essa foi a primeira carta");
+    firstCard = card;
+    firstCard.setAttribute("onclick", "");
+    clicks++;
+  } else if (firstCard.innerHTML === card.innerHTML) {
+    console.log("Cartas iguais");
+    card.setAttribute("onclick", "");
+    firstCard.setAttribute("onclick", "");
+    firstCard = null;
+    clicks++;
+    pairs = pairs + 2;
+    console.log(pairs);
+    if (pairs === numberOfCards) {
+      alert(`Você ganhou em ${clicks / 2} jogadas!`);
+      return;
+    }
+  } else {
+    console.log("Cartas Diferentes");
+    differentCards(card, firstCard);
+    firstCard = null;
+    clicks++;
+  }
+}
+
+function differentCards(card, firstCard) {
+  setTimeout(() => unflipCard(card, firstCard), 1000);
+
+  function unflipCard(card, firstCard) {
+    const frontCard = card.querySelector(".front");
+    const backCard = card.querySelector(".back");
+    frontCard.classList.remove("hide");
+    backCard.classList.add("hide");
+    card.setAttribute("onclick", "toSelectCard(this)");
+    const frontFirstCard = firstCard.querySelector(".front");
+    const backFirstCard = firstCard.querySelector(".back");
+    frontFirstCard.classList.remove("hide");
+    backFirstCard.classList.add("hide");
+    firstCard.setAttribute("onclick", "toSelectCard(this)");
+  }
 }
